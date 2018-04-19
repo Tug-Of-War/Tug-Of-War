22bd6011046264c715bfccf9f37453a76dcd0b24
rg872 <gilang0293@gmail.com>
2018-04-20 02:57:09 +0700

    udah lumayan responsive, tambah animasi pakai animate.css

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -2,38 +2,39 @@
   <div>
     <div class="row" v-if="choose_team">
         <div class="col-5">
-            <button class="button btn-primary btn-lg" @click="pickSide('blue')">Join Team Blue</button>
+            <button class="button btn-primary btn-lg animated bounceInLeft" @click="pickSide('blue')">Join Team Blue</button>
         </div>
-        <div id="start" class="col-2">
-            
+        <div id="side" class="col-2">
+            <h1 class="animated zoomInDown">Choose Your Side</h1>
         </div>
         <div class="col-5">
-            <button class="button btn-danger btn-lg" @click="pickSide('red')">Join Team Red</button>
+            <button class="button btn-danger btn-lg animated bounceInRight" @click="pickSide('red')">Join Team Red</button>
         </div>
     </div>  
     <div class="row" v-else>
-        <div class="col-5">            
-            <div class="card" v-for="(memberBlue,index) in team1" :key="index">
-                <img class="card-img-top" src="@/assets/photofunky.gif">
+        <div class="col-5 animated slideInDown">            
+            <div class="card" v-for="(memberBlue,index) in team1" :key="index" style="background-color: rgba(0, 0, 255, 0.7);">
+                <img class="card-img-top" src="@/assets/img/photofunky.gif">
                 <div class="card-body">
-                    <h5 class="card-title">{{ memberBlue }}</h5>
+                    <h5 class="card-title">{{ truncateName(memberBlue) }}</h5>
                 </div>
             </div>
         </div>
         <div id="start" class="col-2">
-            <img src="@/assets/PleaseWait.gif">
+            <img src="@/assets/img/PleaseWait.gif" v-if="!start_game">
+            <img src="@/assets/img/countdown.gif" v-else>
         </div>
-        <div class="col-5">            
-            <div class="card" v-for="(memberRed,index) in team2" :key="index">
-                <img class="card-img-top" src="@/assets/photofunky.gif">
+        <div class="col-5 animated slideInUp">            
+            <div class="card" v-for="(memberRed,index) in team2" :key="index" style="background-color: rgba(255, 0, 0, 0.7);">
+                <img class="card-img-top" src="@/assets/img/photofunky.gif">
                 <div class="card-body">
-                    <h5 class="card-title">{{ memberRed }}</h5>
+                    <h5 class="card-title">{{ truncateName(memberRed) }}</h5>
                 </div>
             </div>
         </div>
     </div>
     <div class="d-flex justify-content-center" v-if="!choose_team" style="margin-bottom:10%;">
-        <button class="button btn-warning btn-lg">Quit Room ?</button>
+        <button class="button btn-warning btn-lg">Quit Room</button>
     </div>
   </div>
   
@@ -46,12 +47,15 @@ export default {
         return{
             user: 'Joko',
             choose_team: true,
-            team1:['Rio','Roy','Rex','Ronasdasdasdasdasdasdasda'],
+            start_game: false,
+            team1:['Rio','Roy','Rex','Ronasdasdasdasdasdasdasda','Raid'],
             team2:['Jax','June','Jade','Joy'],
         }
     },
+
     methods: {
-        pickSide(side) {
+        pickSide: function(side) {
+
             if(side === 'blue'){
                 this.team1.push(this.user)
                 this.choose_team = false;
@@ -59,8 +63,19 @@ export default {
                 this.team2.push(this.user)
                 this.choose_team = false;
             }
+        },
+
+         truncateName: function(name){
+            if(name.length > 6){
+                name = name.slice(0,6) + '...'
+                return name
+            }else{
+                return name
+            }
         }
+        
     },
+
     created: function() {
         if(this.team1.length === 5) {
             this.team2.push(this.user)
@@ -69,16 +84,57 @@ export default {
             this.team1.push(this.user)
             this.choose_team = false;
         }
+    },
+
+    watch:{
+        team1: function(team1){
+            let _this = this;
+            
+            let players_total = team1.length + this.team2.length;
+
+            if(players_total === 10){
+                this.start_game = true;
+
+                setTimeout(function(){ 
+                    _this.$router.push('/')
+                 }, 5000);
+            }
+            
+        },
+
+        team2: function(team2){
+            let _this = this;
+            
+            let players_total = team2.length + this.team1.length;
+
+            if(players_total === 10){
+                this.start_game = true;
+
+                setTimeout(function(){ 
+                    _this.$router.push('/')
+                 }, 5000);
+            }            
+        }
+    },
+
+    computed:{
+       
     }
 }
 </script>
 
 <style scoped>   
+    #side{
+        height: fit-content;
+        width: fit-content;
+        margin-top: 10%;        
+        margin-bottom: 10%;
+    }
 
     #start{
         height: fit-content;
         width: fit-content;
-        margin-top: 20%;
+        margin-top: 30%;
     }
 
     #start img{
@@ -88,12 +144,11 @@ export default {
     .card{
         height: fit-content;
         width: 30%;
-        margin: 2%;
-        background-color: rgba(255, 255, 255, 0.411);
+        margin: 2%;        
     }
 
     .card-title{
-        font-size: 1rem;
+        font-size: 1.5rem;
     }
 
     .col-5{
@@ -107,4 +162,38 @@ export default {
         align-items: center;
         justify-content: center;
     }
+
+    .col-2 h1{
+        visibility: unset;
+    }
+
+    
+    @media only screen and (max-width: 820px) {
+        .card-title{
+            font-size: 1rem;
+        }
+    }
+
+    @media only screen and (max-width: 675px) {
+        .card-title{
+            font-size: 0.81rem;
+        }
+    }
+
+    @media only screen and (max-width: 620px) {
+        .card-title{
+            font-size: 0.8rem;
+            width: 2%;
+        }
+
+        .col-5{
+            display: flex;
+            flex-direction: column;
+            align-items: center;        
+        }
+
+        .col-2{
+            visibility: hidden;
+        }
+    } 
 </style>
