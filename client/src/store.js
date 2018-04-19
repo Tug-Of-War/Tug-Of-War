576b298af32e642e1b9860648b2c56413c48d33a
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-04-19 21:21:58 +0700

    lobby-page

--- client/src/store.js
+++ client/src/store.js
@@ -8,10 +8,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    rooms: [{
+      id: 1,
+      status: 'ready',
+      count: 50,
+      member: 5,
+      master: 'rama'
+    }, {
+      id: 2,
+      status: 'ready',
+      count: 50,
+      member: 7,
+      master: 'dimas'
+    }, {
+      id: 3,
+      status: 'ready',
+      count: 50,
+      member: 10,
+      master: 'dimas'
+    }],
+    teams: [],
+    users: [{
+      id: 1,
+      nickname: 'rama'
+    }, {
+      id: 2,
+      nickname: 'rama1'
+    }, {
+      id: 3,
+      nickname: 'rama2'
+    }, {
+      id: 4,
+      nickname: 'rama3'
+    }, {
+      id: 5,
+      nickname: 'rama4'
+    }, {
+      id: 6,
+      nickname: 'rama5'
+    }]
   },
   mutations: {
-
+    joinGroup (group) {
+      // masukin user ini ke group id
+      console.log('masuk sini', group)
+    },
+    addRoom (newRoom) {
+      // nambahin room dengan masternya si user
+      console.log('add room', newRoom)
+    }
   },
   actions: {
 
