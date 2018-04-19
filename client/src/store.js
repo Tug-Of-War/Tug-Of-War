8fc473e57c31787e68f004cd3e99b62a21b245fb
Wikanyaa <Mac@Macbooknyaa.localdomain>
2018-04-19 19:58:56 +0700

    add firebase

--- client/src/store.js
+++ client/src/store.js
@@ -2,18 +2,59 @@ import './firebase'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueFire from 'vuefire'
+import firebase from 'firebase'
 
 Vue.use(VueFire)
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    room: '',
+    username: '',
+    side: '',
+    counter: 0,
+    winner: ''
   },
   mutations: {
-
+    addRoom: function (state, payload) {
+      state.room = payload
+      console.log(state.room)
+    },
+    addUser: function (state, payload) {
+      state.username = payload
+      console.log(state.username)
+    },
+    selectSide: function (state, payload) {
+      state.side = payload
+      console.log(state.side)
+    },
+    counter: function (state, payload) {
+      state.counter = payload
+    },
+    winner: function (state, payload) {
+      if (state.counter === 10) {
+        state.winner = 'a'
+      } else if (state.counter === -10) {
+        state.winner = 'b'
+      }
+    }
   },
   actions: {
-
+    startGame: function ({state, commit}, payload) {
+      firebase.database().ref().child('room/' + state.room + '/' + state.username).set({
+        username: state.username,
+        side: state.side
+      })
+      localStorage.setItem('side', state.side)
+      localStorage.setItem('username', state.username)
+      localStorage.setItem('room', state.room)
+    },
+    counter: function ({state, commit}, payload) {
+      let room = localStorage.getItem('room')
+      firebase.database().ref().child('room/' + room).update({
+        counter: payload
+      })
+      commit('counter', payload)
+    }
   }
 })
